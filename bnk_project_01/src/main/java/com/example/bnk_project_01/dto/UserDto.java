88cf42865868949955b962e78bed2249552d8c47
YujinKKK <k3jin41@naver.com>
2025-07-07 15:37:46 +0900

    Test
    [YJ] TEST

--- bnk_project_01/src/main/java/com/example/bnk_project_01/dto/UserDto.java
+++ bnk_project_01/src/main/java/com/example/bnk_project_01/dto/UserDto.java
@@ -1,6 +1,15 @@
 package com.example.bnk_project_01.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
 
 	private String username;
+	private String password;
+	private String role;
 }
