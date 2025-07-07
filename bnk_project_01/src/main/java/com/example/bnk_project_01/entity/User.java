1245802a98cbbd0253d0c649a9be1c049c47194e
Minsoo Kim <mingsoo91@gmail.com>
2025-07-07 15:08:06 +0900

    UserEntity

--- bnk_project_01/src/main/java/com/example/bnk_project_01/entity/User.java
+++ bnk_project_01/src/main/java/com/example/bnk_project_01/entity/User.java
@@ -1,8 +1,17 @@
 package com.example.bnk_project_01.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "bnk_user")
 public class User {
-
+	
+	@Id
+	private String username;
+	
+	private String password;
+	
+	private String role = "ROLE_USER";
 }
