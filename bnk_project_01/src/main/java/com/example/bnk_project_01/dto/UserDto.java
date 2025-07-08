56f85396c71642611268aba8b0c5275c43660d6e
YujinKKK <k3jin41@naver.com>
2025-07-08 11:43:34 +0900

    [yjk] conflict

--- bnk_project_01/src/main/java/com/example/bnk_project_01/dto/UserDto.java
+++ bnk_project_01/src/main/java/com/example/bnk_project_01/dto/UserDto.java
@@ -1,25 +1,13 @@
 package com.example.bnk_project_01.dto;
 
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class UserDto {
 
 	private String username;
 	private String password;
 	private String role;
-
-import lombok.Data;
-
-
-public class UserDto {
-
-    private String username;
-    private String password;
 
 }
