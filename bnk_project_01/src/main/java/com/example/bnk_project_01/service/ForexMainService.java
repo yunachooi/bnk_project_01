ddeaa5d59c5b15014b2319f4c7e3bdd57a908928
Yamtan1 <gndndk12305@naver.com>
2025-07-16 17:24:26 +0900

    [DYH]메인페이지 글씨체 통일 완료

--- bnk_project_01/src/main/java/com/example/bnk_project_01/service/ForexMainService.java
+++ bnk_project_01/src/main/java/com/example/bnk_project_01/service/ForexMainService.java
@@ -48,7 +48,7 @@ public class ForexMainService {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    } */
+    } */  //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@서버 끄면 주석처리해놓으세요@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     @Value("${exim.api-key}")
     private String authKey;
 
